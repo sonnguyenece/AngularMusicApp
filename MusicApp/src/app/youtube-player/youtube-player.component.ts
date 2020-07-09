2cdfaf612e4cf108df3554afea487bab039dc0de
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-07-09 12:00:48 +0700

    init music app

--- MusicApp/src/app/youtube-player/youtube-player.component.ts
+++ MusicApp/src/app/youtube-player/youtube-player.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {DomSanitizer} from "@angular/platform-browser";
+import {YoutubeService} from "../youtube.service";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-youtube-player',
   templateUrl: './youtube-player.component.html',
   styleUrls: ['./youtube-player.component.scss']
 })
-export class YoutubePlayerComponent implements OnInit {
+export class YoutubePlayerComponent implements OnInit, OnDestroy {
+  song: any;
+  sub: Subscription;
+  constructor(
+    private youtubeService: YoutubeService,
+    private activatedRouter: ActivatedRoute,
+    private domSanitizer: DomSanitizer
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.sub = this.activatedRouter.paramMap.subscribe((paramMap: ParamMap) => {
+      const id = paramMap.get('id');
+      this.song = this.youtubeService.find(id);
+    });
+  }
 
-  ngOnInit(): void {
+  getSrc() {
+    const url = 'https://www.youtube.com/embed/' + this.song.id;
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
+
--- MusicApp/src/app/youtube-playlist/youtube-playlist.component.ts
+++ MusicApp/src/app/youtube-playlist/youtube-playlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {YoutubeService} from "../youtube.service";
 
 @Component({
   selector: 'app-youtube-playlist',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YoutubePlaylistComponent implements OnInit {
 
-  constructor() { }
+  constructor(public youtubeService: YoutubeService) { }
 
   ngOnInit(): void {
   }
